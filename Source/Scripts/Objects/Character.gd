586651e46db42e7a46cd013f8d1da19f13d23dbc
dschein1 <32875131+dschein1@users.noreply.github.com>
2020-07-13 14:59:49 +0300

    made base for navigation
    added the navigation to the tile map
    added base variables to controlable objects

--- Source/Scripts/Objects/Character.gd
+++ Source/Scripts/Objects/Character.gd
@@ -2,7 +2,7 @@ extends "res://Source/Scripts/Objects/ControllableObject.gd"
 
 var acceleration
 var max_speed
-
+var TURN_SPEED = 0.5
 var velocity
 
 func _init(acc, max_sp):
@@ -16,5 +16,5 @@ func move(input, delta):
 		velocity = velocity.move_toward(input * max_speed, acceleration * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, Constants.FRICTION * delta)
-	rotation += (get_local_mouse_position().angle()+PI/2) *0.5
+	rotation += (get_local_mouse_position().angle()+PI/2) *TURN_SPEED
 	velocity = move_and_slide(velocity)
--- Source/Scripts/Objects/ControllableObject.gd
+++ Source/Scripts/Objects/ControllableObject.gd
@@ -1,1 +1,4 @@
 extends KinematicBody2D
+
+export var is_active = true
+export var is_controlled = false
