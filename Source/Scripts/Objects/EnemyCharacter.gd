7baddf489a9ef427c8d3cd684b91e5a2b4e2b9d7
dschein1 <32875131+dschein1@users.noreply.github.com>
2020-07-14 14:06:21 +0300

    finished navigation
    turns are sharp
    navigation works
    art is now 32 by 32(but wall tiles should stay)

--- Source/Scripts/Objects/EnemyCharacter.gd
+++ Source/Scripts/Objects/EnemyCharacter.gd
@@ -22,25 +22,29 @@ func move_on_path(delta):
 	if move_vector.length() < min_distance:
 		path_index += 1
 	else:
-		speed = clamp(speed+ acceleration,0,max_speed)
+		speed = clamp(speed + acceleration,0,max_speed)
 		move_and_slide(move_vector.normalized() * speed)
-		look_at(lerp(Vector2(cos(rotation), sin(rotation)), path_points[path_index] + move_vector.normalized(), 0.5))
-	#var current_location = to_global(position)
-	#animationTree.set("parameters/blend_position", vector)
-	#for i in range(path_points.size()):
-		#var next = to_global(path_points[0])
-	#need to check whether the point is far enough. if it's close, remove it and move to next
-	#if it isn't close, move and slide towards it.
-		#if (next - current_location).length() > min_distance:
-			#var direction = (next - current_location).normalized()
-			#speed = clamp(speed+ acceleration,0,max_speed)
-			#velocity = direction * speed
-			#rotation += get_angle_to(next)
-			#var actual_vel = move_and_slide(velocity)
-			#print(global_rotation)
-			#global_rotation = global_position.angle_to_point(next)
+		look_at(path_points[path_index]+move_vector)
+
+#		var facing_vector = to_global(Vector2(cos(rotation), sin(rotation)))
+#		look_at(facing_vector.linear_interpolate(path_points[path_index], 0.1))
+#	look_at(lerp(Vector2(cos(rotation), sin(rotation)), path_points[path_index] + move_vector.normalized(), 0.5))
+#	var current_location = to_global(position)
+#	animationTree.set("parameters/blend_position", vector)
+#	for i in range(path_points.size()):
+#		var next = to_global(path_points[0])
+#	need to check whether the point is far enough. if it's close, remove it and move to next
+#	if it isn't close, move and slide towards it.
+#		if (next - current_location).length() > min_distance:
+#			var direction = (next - current_location).normalized()
+#			speed = clamp(speed+ acceleration,0,max_speed)
+#			velocity = direction * speed
+#			rotation += get_angle_to(next)
+#			var actual_vel = move_and_slide(velocity)
+#			print(global_rotation)
+#			global_rotation = global_position.angle_to_point(next)
 #			if check_if_can_turn(position.angle_to(next)):
 #				global_rotation += position.angle_to(next)
-			#break
-		#else:
-			#path_points.remove(0)
+#			break
+#		else:
+#			path_points.remove(0)
