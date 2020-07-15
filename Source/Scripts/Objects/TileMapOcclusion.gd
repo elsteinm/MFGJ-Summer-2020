5a05269f1b6f6bd68e79b0f71576668930998271
dschein1 <32875131+dschein1@users.noreply.github.com>
2020-07-15 17:50:59 +0300

    added auto generate tiles

---
+++ Source/Scripts/Objects/TileMapOcclusion.gd
@@ -0,0 +1,27 @@
+tool
+extends TileMap
+
+export(bool) var generate = false setget generate_occlusion
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func generate_occlusion(value):
+	var used = get_used_rect()
+	print("before: " + str(used))
+	used.position = used.position - Vector2(4,4)
+	used.size = used.size + Vector2(8,8)
+	print("after: " + str(used))
+	for i in range(used.size.x):
+		for j in range(used.size.y):
+			if get_cell(used.position.x + i,used.position.y + j) != 0:
+				set_cell(i + used.position.x,used.position.y + j,1)
