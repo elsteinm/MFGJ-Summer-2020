a220304ede8364ef44dca06687fa1ba36075b785
elsteinm <eladsteinmetz0@gmail.com>
2020-07-20 19:50:48 +0300

    Music with distance modifier

--- Source/Scripts/Global Scripts/AudioPlayer.gd
+++ Source/Scripts/Global Scripts/AudioPlayer.gd
@@ -3,9 +3,18 @@ extends Node
 onready var music_player = $MusicPlayer
 onready var sfx_player = $SFXPlayer
 
+var music_bus
+var sfx_bus
+
+var music_pitch setget set_music_pitch
+
+func _ready():
+	music_bus = AudioServer.get_bus_index("MusicBus")
+	sfx_bus = AudioServer.get_bus_index("SFXBus")
+	self.music_pitch = 0.8
+
 func play_music(music):
 	music_player.stream = music
-	music_player.pitch_scale = 0.8
 	music_player.play()
 
 func stop_music():
@@ -14,3 +23,9 @@ func stop_music():
 func play_effect(effect):
 	sfx_player.stream = effect
 	sfx_player.play()
+
+func set_music_pitch(value):
+	music_pitch = value
+	music_player.pitch_scale = music_pitch
+	var pitch_effect = AudioServer.get_bus_effect(music_bus, 0)
+	pitch_effect.pitch_scale = float(1)/float(value)
--- Source/Scripts/Level.gd
+++ Source/Scripts/Level.gd
@@ -1,17 +1,33 @@
 extends Node2D
 
+onready var player = $PlayerCharacter
+onready var tween = $Tween
+
 var level_music = load("res://Resources/Audio/Music/Kevin MacLeod - Envision.ogg")
 
 var objects : Array = []
 var object_number = 0
 var objects_dimmed = 0
 
+var enemies
+
 func _init():
 	Main.current_level = self
 	PlayerInput.playing = true
 
 func _ready():
-	get_tree().call_group("enemy", "get_nav",$Navigation2D)
+	get_tree().call_group("enemy", "get_nav", $Navigation2D)
+	enemies = get_tree().get_nodes_in_group("enemy")
+
+func _process(_delta):
+	var shortest_distance = 150
+	for enemy in enemies:
+		if enemy.is_active == true:
+			var distance_from_player = player.global_position.distance_to(enemy.global_position)
+			if distance_from_player < shortest_distance:
+				shortest_distance = distance_from_player
+	var pitch = 0.8 + (float(150 - shortest_distance)/float(100))*0.7
+	AudioPlayer.music_pitch = pitch
 
 #Add object to objectives array
 func add_object(o):
